2a4fb23af7de2c6219d88686f880c85ec9a8923d
Reid Draper <reiddraper@gmail.com>
2013-06-16 19:45:05 -0400

    More properties notes

--- src/simple_check/properties.clj
+++ src/simple_check/properties.clj
@@ -1,6 +1,20 @@
 (ns simple-check.properties
   (:require [simple-check.generators :as gen]))
 
+;; The fields that should be returned from each test
+;; run:
+;;
+;; pass?: boolean or nil (nil implies test was discarded)
+;; expect: boolean or (maybe?) exception
+;; values: the realized values during this test
+;;
+;;
+;; These fields could be returned:
+;;
+;; interrupted?
+;; stamp: haskell QC nomenclature for stats/values collected
+;; callbacks: maybe this is where printing/clojure.test stuff goes?
+
 (defn apply-gen
   [function]
   (fn [args]
