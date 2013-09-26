4da80712c0a3e6831cf6b53724a653372ebcf271
Reid Draper <reiddraper@gmail.com>
2013-09-25 22:14:44 -0500

    Prepare for 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reiddraper/simple-check "0.4.1"
+(defproject reiddraper/simple-check "0.4.2-SNAPSHOT"
   :description "A QuickCheck inspired property-based testing library."
   :url "http://github.com/reiddraper/simple-check"
   :license {:name "Eclipse Public License"
