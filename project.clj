3a6d58ddeb9b0870bbadca96c7570deb86c41cd6
Reid Draper <reiddraper@gmail.com>
2013-11-05 19:44:34 -0600

    Remove runtime dependencies
    simple-check only needs Clojure and codox-md for development. Since
    simple-check is a library, users will already have Clojure as a
    dependency.

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://github.com/reiddraper/simple-check"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.0"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :global-vars {*warn-on-reflection* true}
   :codox {:writer codox-md.writer/write-docs}
   :plugins [[codox "0.6.4"]])
