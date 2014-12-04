27ee1de574b42a52de7a2923621c50c032e8712c
Gary Fredericks <fredericksgary@gmail.com>
2014-12-03 21:47:03 -0600

    Refactor tests for readability

--- src/test/clojure/clojure/test/check/random_test.clj
+++ src/test/clojure/clojure/test/check/random_test.clj
@@ -4,16 +4,29 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.random :as random]))
 
-(def gen-split-operation
-  (gen/elements [(comp first random/split)
-                 (comp second random/split)]))
+(def gen-split-steps
+  (gen/list (gen/elements [:left :right])))
+
+(defn apply-split-steps
+  [rng steps]
+  (if-let [[step & more] (seq steps)]
+    (let [[rng1 rng2] (random/split rng)]
+      (recur (case step :left rng1 :right rng2) more))
+    rng))
+
+(def gen-splitter
+  "Generates a function that will repeatedly split a rng in random
+  directions."
+  (gen/fmap #(apply comp %) (gen/list gen-split-operation)))
+
+(def gen-seed (gen/choose 0 Integer/MAX_VALUE))
 
 (defspec determinism-spec
   ;; kind of weird trying to generate a random seed; gen/nat would
   ;; obviously be pretty bad.
-  (prop/for-all [seed (gen/choose 0 Integer/MAX_VALUE)
-                 ops (gen/list gen-split-operation)]
+  (prop/for-all [seed gen-seed
+                 steps gen-split-steps]
     (let [r1 (random/make-random seed)
-          r2 (random/make-random seed)
-          func (apply comp random/rand-long ops)]
-      (= (func r1) (func r2)))))
+          r2 (random/make-random seed)]
+      (= (-> r1 (apply-split-steps steps) (random/rand-long))
+         (-> r2 (apply-split-steps steps) (random/rand-long))))))
