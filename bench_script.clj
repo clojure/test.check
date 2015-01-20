0f383f7d4a5ddc71563368ac6c9e807346376cb3
Gary Fredericks <fredericksgary@gmail.com>
2015-01-19 20:18:11 -0600

    Add rng benchmarks to the script

--- bench_script.clj
+++ bench_script.clj
@@ -16,3 +16,18 @@
 (println "======= Pasted AES =======")
 (let [f @#'r/fast-aes]
   (bench (f k in out)))
+
+(println "======= java.util.Random =======")
+(let [r (java.util.Random. 42)]
+  (bench (loop [i 256 sum 0]
+           (if (zero? i)
+             sum
+             (recur (dec i) (bit-xor sum (.nextLong r)))))))
+
+(println "======= AES generation =======")
+(let [r (random/make-random 42)]
+  (bench (loop [i 256, sum 0, r r]
+           (if (zero? i)
+             sum
+             (let [[r1 r2] (random/split r)]
+               (recur (dec i) (bit-xor sum (random/rand-long r1)) r2))))))
