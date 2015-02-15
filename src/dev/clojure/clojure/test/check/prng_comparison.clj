6750839ffa054a79dc0421ea442046d209980b65
Gary Fredericks <fredericksgary@gmail.com>
2015-02-15 10:30:55 -0600

    Clean up prng-comparison namespace
    Now we just have a list of splittable impls and linearization
    strategies

--- src/dev/clojure/clojure/test/check/prng_comparison.clj
+++ src/dev/clojure/clojure/test/check/prng_comparison.clj
@@ -1,8 +1,6 @@
 (ns clojure.test.check.prng-comparison
-  (:require [clojure.test.check.random :as r]
-            [criterium.core :as criterium])
+  (:require [clojure.test.check.random :as r])
   (:import [java.util Random]
-           [org.apache.commons.math3.stat.inference ChiSquareTest]
            [sun.misc Signal SignalHandler]))
 
 ;; Make the process quit when STDOUT stops being read
@@ -11,68 +9,17 @@
                  (handle [_ _]
                    (System/exit 0))))
 
-(def impls
-  {:naive-JUR
-   (fn self1 [^long seed]
-     (let [r (Random. seed)
-           x1 (.nextLong r)
-           x2 (.nextLong r)]
-       ;; no reason to think this will work well
-       (reify r/IRandom
-         (split [_] [(self1 x1) (self1 x2)])
-         (rand-long [_] x1))))
-   :ad-hoc-JUR
-   ;; try to do slightly better than naive-JUR by doing ad-hoc tricks
-   (fn self2 [^long seed]
-     (let [r (Random. seed)
-           x1 (.nextLong r)
-           x2 (.nextLong r)
-           x3 (.nextLong r)
-           x4 (.nextLong r)]
-       (reify r/IRandom
-         (split [_] [(self2 (bit-xor x1 x3)) (self2 (bit-xor x2 x4))])
-         (rand-long [_] x1))))
-   ;; Expecting AES to be high quality by all measures, but poor
-   ;; performance.
-   :AES
+(def splittable-impls
+  {:AES
    (fn [^long seed] (r/make-aes-random seed seed))
    :siphash
    (fn [^long seed] (r/make-siphash-random seed))})
 
-(defn stats
-  [xs]
-  (let [c (double (count xs))
-        mean (/ (reduce + xs) c)
-        variance (/ (->> xs
-                         (map #(- mean %))
-                         (map #(* % %))
-                         (reduce +))
-                    c)]
-    {:mean mean :variance variance}))
-
-(defn right-linear-longs
+(defn lump
+  "Returns a lazy seq of 2^n longs from the given rng."
   [rng n]
-  (when (pos? n)
-    (lazy-seq
-     (let [[rng1 rng2] (r/split rng)]
-       (cons (r/rand-long rng1)
-             (right-linear-longs rng2 (dec n)))))))
-
-(defn left-linear-longs
-  [rng n]
-  (when (pos? n)
-    (lazy-seq
-     (let [[rng1 rng2] (r/split rng)]
-       (cons (r/rand-long rng2)
-             (left-linear-longs rng1 (dec n)))))))
-
-(defn balanced-longs
-  [rng n]
-  (loop [rngs [rng]
-         count 1]
-    (if (>= count n)
-      (take n (map r/rand-long rngs))
-      (recur (mapcat r/split rngs) (* 2 count)))))
+  (map r/rand-long
+       (nth (iterate #(mapcat r/split %) [rng]) n)))
 
 (defn fibonacci-longs
   "Generates n longs from the given rng in a fashion that is somewhere
@@ -94,101 +41,43 @@
                       (fibonacci-longs rng-right right-count not-left-side-heavy?)))
             (recur b (+ a b))))))))
 
-(def extractors
-  {:left-linear (fn [rng-fn seed n] (left-linear-longs (rng-fn seed) n))
-   :right-linear (fn [rng-fn seed n] (right-linear-longs (rng-fn seed) n))
-   :balanced (fn [rng-fn seed n] (balanced-longs (rng-fn seed) n))
-   :fibonacci (fn [rng-fn seed n] (fibonacci-longs (rng-fn seed) n))
-   :sequential-seeds (fn [rng-fn seed n]
-                       (->> (iterate inc seed)
-                            (take n)
-                            (map rng-fn)
-                            (map r/rand-long)))})
-
-(defn chi-square-test
-  [expected-value actuals]
-  (let [expecteds (into-array Double/TYPE (repeat (count actuals) expected-value))]
-    (.chiSquareTest (ChiSquareTest.)
-                    ^doubles expecteds
-                    ^longs (into-array Long/TYPE actuals))))
-
-(def some-seeds
-  "These are all good seeds."
-  [42 1421867162723 -1539333844256672508])
-
-(defn bin-test
-  [n xs]
-  (let [expected (/ (count xs) (double n))
-        freqs (->> xs (map #(mod % n)) (frequencies))
-        actuals (->> (range n) (map #(get freqs % 0)))
-        p (chi-square-test expected actuals)]
-    (if (<= 0.05 p 0.95) :pass :fail)))
-
-(def tests
-  "Functions that take a collection of supposedly independent random
-  longs and return :pass or :fail."
-  {:bin100 (partial bin-test 100)
-   :bin101 (partial bin-test 101)
-   :bin20000 (partial bin-test 20000)})
-
-(defn quality-tests
-  []
-  (println "Running all quality tests...")
-  (doseq [[impl-name impl] impls
-          [extractor-name extractor-fn] extractors
-          [test-name test-fn] tests]
-    (let [seeds (->> some-seeds
-                     (map #(iterate inc %))
-                     (apply interleave)
-                     (take 30))
-          {:keys [pass fail] :or {pass 0 fail 0}}
-          (->> seeds
-               (map #(extractor-fn impl % 200000))
-               (map test-fn)
-               (frequencies))]
-      (printf "%15s -- %16s -- %10s -- %.2f\n"
-              (name impl-name)
-              (name extractor-name)
-              (name test-name)
-              (double (/ pass (+ pass fail))))
-      (flush))))
-
-(defn performance-tests
-  []
-  ;; eventually figure out a way to do a separate jvm run for each one
-  ;; of these?
-  (println "Running performance tests...")
-  (let [{[mean] :mean} (criterium/benchmark
-                        (let [r (java.util.Random. 42)]
-                          (loop [i 1024 x 0]
-                            (if (zero? i)
-                              x
-                              (recur (dec i) (bit-xor x (.nextLong r))))))
-                        {})]
-    (printf "Mutable JUR generating 1024 longs averaged %.2fµs\n"
-            (* mean 1000000)))
-  (let [rng (r/make-siphash-random 42)
-        {[mean] :mean} (criterium/benchmark
-                        (loop [i 1024 x 0 rng rng]
-                          (if (zero? i)
-                            x
-                            (let [[rng1 rng2] (r/split rng)]
-                              (recur (dec i) (bit-xor x (r/rand-long rng1)) rng2))))
-                        {})]
-    (printf "Low-allocation siphash generated 1024 longs averaged %.2fµs\n"
-            (* mean 1000000)))
-  (doseq [[impl-name impl] impls
-          [extractor-name extractor-fn] extractors]
-    (let [{[mean] :mean} (criterium/benchmark
-                          (reduce bit-xor (extractor-fn impl 42 1024))
-                          {})]
-      (printf "%15s[%16s]: Generating 1024 longs averaged %.2fµs\n"
-              (name impl-name)
-              (name extractor-name)
-              (* mean 1000000))
-      (flush))))
-
-(defn do-everything
-  []
-  (quality-tests)
-  (performance-tests))
+(def linearization-strategies
+  ;; have to use different self names here because weird compiler bug
+  {:right-linear
+   (fn self1 [rng]
+     (lazy-seq
+      (let [[rng1 rng2] (r/split rng)]
+        (cons (r/rand-long rng1) (self1 rng2)))))
+   :left-linear
+   (fn self2 [rng]
+     (lazy-seq
+      (let [[rng1 rng2] (r/split rng)]
+        (cons (r/rand-long rng2) (self2 rng1)))))
+   :alternating
+   (fn self3 [rng]
+     (lazy-seq
+      (let [[rng1 rng2] (r/split rng)
+            [rng3 rng4] (r/split rng1)]
+        (cons (r/rand-long rng2)
+              (cons (r/rand-long rng3)
+                    (self3 rng4))))))
+   ;; these two return "effectively" infinite seqs
+   :balanced-63
+   (fn [rng] (lump rng 63))
+   ;; this one should require twice as many calls to siphash as
+   ;; balanced-63, so it's probably slower
+   :balanced-64
+   (fn [rng] (lump rng 64))
+   :right-lumpy
+   (fn self4 [rng]
+     (lazy-seq
+      (let [[rng1 rng2] (r/split rng)]
+        (concat (lump rng1 8) (self4 rng2)))))
+   :left-lumpy
+   (fn self5 [rng]
+     (lazy-seq
+      (let [[rng1 rng2] (r/split rng)]
+        (concat (lump rng2 8) (self5 rng1)))))
+   :fibonacci
+   (let [infinity 1152921504606846976]
+     (fn [rng] (fibonacci-longs rng infinity)))})
