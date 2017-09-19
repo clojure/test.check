489d54d10ddf03b3dcc704be223c35529e9a9cd1
Gary Fredericks <fredericksgary@gmail.com>
2017-09-19 08:16:08 -0500

    Dynamically extend cljs.test/assert-expr
    The clojure.test.check.clojure-test.assertions.cljs namespace exists
    only for the side effect of extending the cljs.test/assert-expr
    multimethod.
    
    This has to be done on the clj side of cljs compilation, and so we have
    a separate namespace that is only loaded by cljs via a :require-macros
    clause in clojure.test.check.clojure-test.assertions. This means we have
    a clj namespace that should only be loaded by cljs compilation.
    
    Normally this is not a problem, but it seems that the clojure maven
    plugin will eagerly load all source files it can find. I'm not sure why
    `cljs.test` is not available in that context, but this seems to fix it.

--- src/main/clojure/clojure/test/check/clojure_test/assertions/cljs.clj
+++ src/main/clojure/clojure/test/check/clojure_test/assertions/cljs.clj
@@ -1,8 +1,11 @@
-(ns clojure.test.check.clojure-test.assertions.cljs
-  (:require
-   [cljs.test]
-   [clojure.test.check.clojure-test.assertions :as assertions]))
+(ns clojure.test.check.clojure-test.assertions.cljs)
 
-(defmethod cljs.test/assert-expr 'clojure.test.check.clojure-test/check?
-  [_ msg form]
-  (assertions/check? msg form))
+(try
+  (require 'cljs.test
+           '[clojure.test.check.clojure-test.assertions :as assertions])
+
+  (eval
+   '(defmethod cljs.test/assert-expr 'clojure.test.check.clojure-test/check?
+      [_ msg form]
+      (assertions/check? msg form)))
+  (catch java.io.FileNotFoundException e))
