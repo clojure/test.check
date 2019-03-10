3d48d5189edb65b99e3c2ec0f399a9b87a682472
Gary Fredericks <fredericksgary@gmail.com>
2019-03-10 16:44:24 -0500

    Better version in the project.clj
    This matches the pom.xml at least.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/test.check "0.9.1-SNAPSHOT"
+(defproject org.clojure/test.check "0.10.0-SNAPSHOT"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
