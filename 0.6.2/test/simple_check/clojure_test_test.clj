a857df7b2f9cf6d2fb8f5288d3ad7302acac3b13
Gary Fredericks <fredericksgary@gmail.com>
2019-06-29 21:15:34 -0500

    Copy 0.6.2 source tree

---
+++ 0.6.2/test/simple_check/clojure_test_test.clj
@@ -0,0 +1,54 @@
+(ns simple-check.clojure-test-test
+  (:use clojure.test)
+  (:require [simple-check.generators :as gen]
+            [simple-check.properties :as prop]
+            [simple-check.clojure-test :as ct :refer (defspec)]))
+
+(defspec trial-counts
+  (prop/for-all* [gen/int] (constantly true))
+  5000)
+
+(defspec long-running-spec
+  (prop/for-all* [] #(do (Thread/sleep 1) true))
+  1000)
+
+(defn- vector-elements-are-unique*
+  [v]
+  (== (count v) (count (distinct v))))
+
+(def ^:private vector-elements-are-unique
+  (prop/for-all*
+    [(gen/vector gen/int)]
+    vector-elements-are-unique*))
+
+(defspec failing-spec vector-elements-are-unique 100)
+
+(defn test-ns-hook
+  []
+  (is (-> (with-out-str (binding [*test-out* *out*] (test-var #'trial-counts)))
+             read-string
+             (select-keys [:test-var :result :num-tests])
+             (= {:test-var 'trial-counts, :result true, :num-tests 5000})))
+  
+  (binding [ct/*report-trials* true]
+     (let [output (with-out-str (test-var #'trial-counts))]
+       (is (re-matches #"(?s)\.{5}.+" output))))
+
+  (binding [ct/*report-trials* ct/trial-report-periodic
+            ct/*trial-report-period* 500]
+    (is (re-seq
+          #"(Passing trial \d{3} / 1000 for .+\n)+"
+           (with-out-str (binding [*test-out* *out*] (test-var #'long-running-spec))))))
+
+  (let [[report-counters stdout]
+        (binding [ct/*report-shrinking* true
+                  ; need to keep the failure of failing-spec from affecting the
+                  ; simple-check test run
+                  *report-counters* (ref *initial-report-counters*)]
+          [*report-counters*
+           (with-out-str (binding [*test-out* *out*] (test-var #'failing-spec)))])]
+    (is (== 1 (:fail @report-counters)))
+    (is (re-seq
+          #"(?s)Shrinking vector-elements-are-unique starting with parameters \[\[.+"
+          stdout))))
+
